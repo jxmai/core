ec3955bf186318c2047b2e9c00daa13e4d63a820
mai <jason.mai@oltis.ca>
2017-06-13 17:17:03 -0500

    Clean up - remove hardcoded data

--- src/main/resources/META-INF/resources/primefaces-extensions/orgchart/3-orgchart-widget.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/orgchart/3-orgchart-widget.js
@@ -11,54 +11,6 @@ PrimeFaces.widget.ExtOrgChart = PrimeFaces.widget.BaseWidget.extend({
 		this.id = cfg.id;
 		var opts = $.extend(true, {}, cfg);
 
-		// Hardcode data for now
-		/*var datascource = {
-			'name' : 'Lao Lao',
-			'title' : 'general manager',
-			'children' : [ {
-				'name' : 'Bo Miao',
-				'title' : 'department manager'
-			}, {
-				'name' : 'Su Miao',
-				'title' : 'department manager',
-				'children' : [ {
-					'name' : 'Tie Hua',
-					'title' : 'senior engineer'
-				}, {
-					'name' : 'Hei Hei',
-					'title' : 'senior engineer',
-					'children' : [ {
-						'name' : 'Pang Pang',
-						'title' : 'engineer'
-					}, {
-						'name' : 'Xiang Xiang',
-						'title' : 'UE engineer'
-					} ]
-				} ]
-			}, {
-				'name' : 'Yu Jie',
-				'title' : 'department manager'
-			}, {
-				'name' : 'Yu Li',
-				'title' : 'department manager'
-			}, {
-				'name' : 'Hong Miao',
-				'title' : 'department manager'
-			}, {
-				'name' : 'Yu Wei',
-				'title' : 'department manager'
-			}, {
-				'name' : 'Chun Miao',
-				'title' : 'department manager'
-			}, {
-				'name' : 'Yu Tie',
-				'title' : 'department manager'
-			} ]
-		};
-		
-		// hardcode data for now
-		opts['data'] = datascource;*/
-
 		opts['data'] = JSON.parse(opts['data']);
 		this.jq.orgchart(opts);
 	}
